5b0c37ab95e397f00d7011072f45c40519be70e4
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-04-08 16:58:38 +0200

    re-initializing udacity react nanodegree project

---
+++ src/AppReact.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import ListMyBooks from './ListMyBooks'
+import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+
+class BooksAppReact extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(()=>({
+          books: [...books]
+        }))
+      })
+  }
+
+  render(){
+    return(
+      <div>
+        <Route exact path='/' render={() => (
+          <ListMyBooks
+            books={this.state.books}
+          />
+        )} />
+        <Route path='/search' render={({ history }) => (
+          <SearchBooks/>
+        )} />
+      </div>
+    )
+  }
+
+}
+export default BooksAppReact
---
+++ src/SearchBooks.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class SearchBooks extends Component {
+
+}
+
+export default SearchBooks
